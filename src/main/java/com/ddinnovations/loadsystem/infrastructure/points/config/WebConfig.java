103085a058e674ad5d6d3d745fe5ad5989c2a4f4
DiegoFelipe7 <diegofelipem99@gmail.com>
2024-03-16 18:40:33 -0500

    Configuracion de json en los errores

--- src/main/java/com/ddinnovations/loadsystem/infrastructure/points/config/WebConfig.java
+++ src/main/java/com/ddinnovations/loadsystem/infrastructure/points/config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.ddinnovations.loadsystem.infrastructure.points.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -14,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS", "DELETE");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
     }
 }
